85b00c9916cef89d6474181ca287b3455df8ea8c
navadeepkiran <kolupoti.kiran@students.iiit.ac.in>
2026-06-21 18:25:17 +0530

    Stop tracking

--- scratch/test_monkeypatch.py
+++
@@ -1,9 +0,0 @@
-import torch
-_original_load = torch.load
-def _patched_load(*args, **kwargs):
-    kwargs['weights_only'] = False
-    return _original_load(*args, **kwargs)
-torch.load = _patched_load
-import transformers
-import transformers.trainer
-print(transformers.trainer.torch.load)
